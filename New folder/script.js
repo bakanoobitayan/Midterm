8ad56241d33abb5ed0a3f10b2142a5a4d64691bd
MARK LUMACANG <45448150+bakanoobitayan@users.noreply.github.com>
2019-01-31 16:26:17 +0800

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,36 @@
+function one(){
+    document.getElementbyID("display").value+="1";
+ 
+}
+function two(){
+    document.getElementbyID("display").value="";
+}
+function three(){
+    document.getElementbyID("display").value="";
+    
+}
+function four(){
+    document.getElementbyID("display").value="";
+  
+function five(){
+    document.getElementbyID("display").value="";
+   
+}
+function six(){
+    document.getElementbyID("display").value="";
+  
+}
+function seven(){
+    document.getElementbyID("display").value="";
+   
+function eight(){
+    document.getElementbyID("display").value="";
+    
+}
+function nine(){
+    document.getElementbyID("display").value="";
+   
+}
+function zero(){
+    document.getElementbyID("display").value="";
+    
